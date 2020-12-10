7a638f3da7f38237ece80542f379967048474e83
koladeg <tayokay@yahoo.com>
2020-12-10 17:44:49 +0100

    first commit

---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+
+const ProductsOverviewScreen = () => {
+    return (
+        <View>
+            <FlatList />
+        </View>
+    )
+}
+
+export default ProductsOverviewScreen
+
+const styles = StyleSheet.create({})
